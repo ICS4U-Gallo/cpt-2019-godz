198dc3969ee3df0e90c58bdb4f5edabd1d75744f
Ryan Kim <43964171+Kimchooman@users.noreply.github.com>
2020-01-13 17:42:15 -0500

    Update map_tool.py

--- map_tool.py
+++ map_tool.py
@@ -1,74 +1,145 @@
 import csv
 import sys
 import os
+import arcade
 
-class Box:
-    def __init__(self, x, y, w, h):
-        self.x = x
-        self.y = y
-        self.width = w
-        self.height = h
+class Box(arcade.Sprite):
+	def __init__(self, x, y):
+		super().__init__(filename="sprites/test_wall_sprite.png", center_x=x, center_y=y)
 
-        self.compact_list = [self.x, self.y, self.width, self.height]
+		self.half_sprite = 10
+		self.x = self.center_x - self.half_sprite
+		self.y = self.center_y - self.half_sprite
+		self.compact_list = [x, y]
 
 class Session_manager:
-    def __init__(self):
-        
-        if len(sys.argv) == 4 and sys.argv[1] == "n": # main.py n w h
+	def __init__(self):
+		
+		self.block_size = 20
+		self.half_block = self.block_size / 2 
 
-            self.session_WIDTH = sys.argv[2]
-            self.session_HEIGHT = sys.argv[3]
-        
-        elif len(sys.argv) == 3 and sys.argv[1] == "l": # main.py l directory
-            
-            self.complete_dir = f"saves/{sys.argv[2]}"
-            self.load_prev_session()
+		self.points = []
+		if len(sys.argv) == 5 and sys.argv[1] == "n": # main.py n filename.csv w h
+			
+			self.WIDTH = int(sys.argv[3])
+			self.HEIGHT = int(sys.argv[4])
+			
+			self.complete_dir = f"levels/{sys.argv[2]}"
+		
+		elif len(sys.argv) == 3 and sys.argv[1] == "l": # main.py l directory
+			
+			self.complete_dir = f"levels/{sys.argv[2]}"
+			self.load_prev_session()
 
-        else:
-            raise ValueError("""
-            Run script by (cpt_map_tool.py n/l) \"n\" for new map, \"l\" for laoding a previous map
-            creating a new map requires file.py n width height
-            loading an old map requires file.py l csv_file.csv """)
+		else:
+			raise ValueError("""
+			Run script by (cpt_map_tool.py n/l) \"n\" for new map, \"l\" for laoding a previous map
+			creating a new map requires file.py n width height
+			loading an old map requires file.py l csv_file.csv """)
 
-        self.unsaved_point = []
-        self.loaded_points = []
+		self.unsaved_changes = False
 
-        self.unsaved_changes = False
+	def load_prev_session(self):
+		
+		with open(self.complete_dir) as csv_file:
+			csv_reader = csv.reader(csv_file, delimiter=",")
 
-    def load_prev_session(self):
-        
-        with open(self.complete_dir) as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter=",")
+			for row in csv_reader:
+				self.WIDTH, self.HEIGHT = int(row[0]), int(row[1])
+				break
 
-        self.session_WIDTH, self.session_HEIGHT = csv_reader[0][0], csv_reader[0][1]
+			count = 0
 
-        for row in range(1, len(csv_reader)):
-            
-            row = csv_reader[row]
+			for row in csv_reader:
+				
+				if count != 0:
+					self.points.append(Box(float(row[0]), float(row[1])))
 
-            self.loaded_points.append(Box(row[0], row[1], row[2], row[3]))
+				else:
+					count += 1
 
-    def save_current_sesssion(self):
+	def save_current_sesssion(self):
 
-        with open(self.complete_dir) as csv_file:
+			with open(self.complete_dir, "w") as csv_file:
+				file_writer = csv.writer(csv_file, delimiter=",")
 
-            csv_writer = csv.writer(csv_file, delimiter=",")
+				file_writer.writerow([self.WIDTH, self.HEIGHT])
 
-            while len(self.unsaved_point) > 0:
+				for box in self.points:
+					file_writer.writerow(box.compact_list)
 
-                csv_writer.writerow(self.unsaved_point[0].compact_list)
 
-                del self.unsaved_point[0]
-    
-    def add_new_box(self, x, y, w, h):
-        
-        if w < 0:
-        
-        if h < 0:
+	def add_new_box(self, x, y):
+		
+		true_x = x - (x % self.block_size)
+		true_y = y - (y % self.block_size)
 
-        self.unsaved_point.append(Box(x, y, w, h))
-    
-    def rm_box(self, x, y):
+		center_x = true_x + self.half_block
+		center_y = true_y + self.half_block
 
-        for box in self.unsaved_point:
-            if 
+		self.points.append(Box(center_x, center_y))
+	
+	def rm_box(self, x, y):
+		
+		count = 0
+		for box in self.points:
+			if  x > box.x and x < (box.x + self.block_size):
+				if y > box.y and y < (box.y + self.block_size):
+					cd Code
+					del self.points[count]
+
+			count += 1
+
+def draw_grid(width, height, spacing):
+
+	for r in range(0, width, spacing):
+		arcade.draw_line(r, 0, r, height, arcade.color.BLACK_LEATHER_JACKET, 2)
+
+	for c in range(0, height, spacing):
+		arcade.draw_line(0, c, width, c, arcade.color.BLACK_LEATHER_JACKET, 2) 
+
+session = Session_manager()
+
+print(session.WIDTH, session.HEIGHT)
+
+window = arcade.open_window(session.WIDTH, session.HEIGHT, "Map Builder")
+
+
+def setup():
+	arcade.set_background_color(arcade.color.WHITE)
+	arcade.schedule(update, 1/60)
+	arcade.run()
+
+def update(delta_time):
+	pass
+
+@window.event
+def on_draw():
+	arcade.start_render()
+	
+	draw_grid(session.WIDTH, session.HEIGHT, session.block_size)
+
+	for box in session.points:
+		box.draw()
+
+@window.event
+def on_key_press(key, modifiers):
+	
+	if key == arcade.key.S:
+		session.save_current_sesssion()
+
+@window.event
+def on_key_release(key, modifiers):
+	pass
+
+@window.event
+def on_mouse_press(x, y, button, modifiers):
+	
+	if button == arcade.MOUSE_BUTTON_LEFT:
+		session.add_new_box(x,y)
+	
+	elif button == arcade.MOUSE_BUTTON_RIGHT:
+		session.rm_box(x,y)
+
+if __name__ == '__main__':
+	setup()

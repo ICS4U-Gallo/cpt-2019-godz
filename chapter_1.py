0ada0418500ab585581ae26931771e79dc3f2b0b
Ryan Kim <43964171+Kimchooman@users.noreply.github.com>
2020-01-13 20:04:18 -0500

    Update chapter_1.py

--- chapter_1.py
+++ chapter_1.py
@@ -9,104 +9,93 @@ PLAYER_JUMP = 20.0
 PLAYER_GRAVITY = 1.0
 
 class Point:
+    """ Convience class representing an x,y cordinate pair"""
+
     def __init__(self,x ,y):
         self.x = x
         self.y = y
 
-class Keyboard_controller:
-    def __init__(self):
-
-        self.A = False
-        self.D = False
-        self.space = False
-
-
-class Platform(arcade.Sprite):
-    n_image_scale = 1
-    platform_size = [20, 20]
-
-    def __init__(self, x, y, filename):
-        
-        self.file_extension = "png"
-        self.path = f"sprites/{filename}.{self.file_extension}"
-
-        super().__init__(center_x=x, center_y=y, filename=self.path)
-
 
 class Platform_manager:
+    """ Class responsible for loading prebuilt levels"""
+
     all = arcade.SpriteList()
 
-    def __init__(self, level_num):
+    def __init__(self, filename):
 
-        self.path = "levels"
-        self.file_extension = "csv"
+        self.csv_path = "levels"
+        self.csv_filename = filename
+        self.csv_path = f"{self.csv_path}/{self.csv_filename}"
 
-        self.complete_path = f"{self.path}/level{level_num}.{self.file_extension}"
+        self.sprite_filename = "test_wall_sprite.png"
+        self.sprite_path = "sprites"
+        self.sprite_path = f"{self.sprite_path}/{self.sprite_filename}"
 
-        with open(self.complete_path) as csv_file:
+        self.sprite_scaling = 1
+
+        with open(self.csv_path) as csv_file:
 
             csv_reader = csv.reader(csv_file, delimiter=",")
 
             count = 0
 
-            for row in csv_reader:
+            # Note, the first row of the csv file is meant to be the map width and height in pixels
+            for row in csv_reader:        
+                if count > 0:
+                    s = arcade.Sprite(center_x=float(row[0]),
+                                    center_y=float(row[1]),
+                                    filename=self.sprite_path,
+                                    scale=self.sprite_scaling)
+                    self.all.append(s)
                 
-                if count != 0:
-                    self.all.append(Platform(float(row[0]), float(row[1]), "test_wall_sprite"))
-                
-                else:
-                    count += 1
-
-
-class Grapple:
-    reduction_factor = 0.1
-
-    def __init__(self,Player_pointer, target_pos):
-
-        self.player_pointer = Player_pointer
-        self.target_pos = target_pos
-    
-    def distance_delta(self):
-        p = self.player_pointer._position
-        
-        delta_x = p.x - self.target_pos.x
-        delta_y = p.y - self.target_pos.y
-
-        return Point(delta_x * self.reduction_factor, delta_y * self.reduction_factor)    
+                else: count += 1
 
 
 class Player(arcade.Sprite):
-    n_image_scale = 2
-    n_player_size = [20, 20]
+    def __init__(self, starting_x, starting_y):
 
-    def __init__(self, sprite_dir, starting_x, starting_y):
-        super().__init__(scale=Player.n_image_scale, filename=sprite_dir, center_x=starting_x, center_y=starting_y)
+        self.sprite_scaling = 1
+        
+        #All caps to prevent the overriding of inhereted attributes
+        self.SPEED = 15.0 
+        self.JUMP = 20.0
+        self.GRAVITY = 1.0
 
-        self.allowing_jumps = True
+        self.character_path = "sprites"
+        self.character_sprite = "test_char_sprite.png"
+        self.character_sprite_path = f"{self.character_path}/{self.character_sprite}"
+
+        super().__init__(filename=self.character_sprite_path,
+                        center_x=starting_x,
+                        center_y=starting_y,
+                        scale=self.sprite_scaling)
+
 
     def screen_location(self):
+        """Returns two x,y pairs represeting the camera view relative to the player"""
+
 
         x = self.center_x - (settings.WIDTH / 2)
         x1 = self.center_x + (settings.WIDTH / 2)
 
         y = self.center_y - (settings.HEIGHT / 2)
-        y1 = self.center_y - (settings.HEIGHT / 2)
+        y1 = self.center_y + (settings.HEIGHT / 2)
 
         return x, x1, y, y1
 
 
 class Chapter1View(arcade.View):
-    def __init__(self, player_pointer):
+    def __init__(self):
 
         super().__init__()
 
-        self.player = player_pointer
+        self.player = Player(starting_x=300 ,starting_y=50)
 
-        self.platform_manager = Platform_manager(1)
+        self.platform_manager = Platform_manager("level1.csv")
 
-        self.key_control = Keyboard_controller()
-
-        self.physics_engine = arcade.PhysicsEnginePlatformer(self.player, self.platform_manager.all, PLAYER_GRAVITY)
+        # Initializing a physics engine instance.
+        self.physics_engine = arcade.PhysicsEnginePlatformer(self.player, self.platform_manager.all)
+        self.physics_engine.gravity_constant = self.player.GRAVITY
 
     def on_show(self):
         arcade.set_background_color(arcade.color.BLUE_SAPPHIRE)
@@ -115,7 +104,6 @@ class Chapter1View(arcade.View):
         arcade.start_render()
     
         self.player.draw()
-   
         self.platform_manager.all.draw()
         
     def on_key_press(self, key, modifiers):
@@ -142,6 +130,7 @@ class Chapter1View(arcade.View):
         
         self.physics_engine.update()
 
+        # Adjusting the viewport (camera)
         x, x1, y, y1 = self.player.screen_location()
         arcade.set_viewport(x, x1, y, y1)
         
@@ -158,10 +147,8 @@ if __name__ == "__main__":
 
     from utils import FakeDirector
     window = arcade.Window(settings.WIDTH, settings.HEIGHT)
-    
-    player = Player("sprites/test_char_sprite.png", 300 ,50)
 
-    my_view = Chapter1View(player)
+    my_view = Chapter1View()
     my_view.director = FakeDirector(close_on_next_view=True)
     window.show_view(my_view)
     arcade.run()

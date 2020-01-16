2dec2636f7f8a3051a34ee786096505e1e5c5863
aryanirani6 <54900949+aryanirani6@users.noreply.github.com>
2020-01-16 11:09:48 -0500

    Update chapter_3.py

--- chapter_3.py
+++ chapter_3.py
@@ -1,6 +1,12 @@
 import arcade
 import settings
 from datetime import datetime
+import time
+
+time_game = 60
+start_time = time.time()
+end_time = time.time()
+
 
 class Chapter3View(arcade.View):
     def __init__(self):
@@ -22,6 +28,20 @@ class Chapter3View(arcade.View):
         self.goalie.center_y = 275
         self.shooter.center_x = 300
         self.shooter.center_y = 50
+        self.radar_speed_x = 2
+        self.radar_speed_y = 0
+        self.start_ball_x = self.ball_center_x
+        self.end_ball_x = self.ball_center_x
+        self.start_ball_y = self.ball_center_y
+        self.end_ball_y = self.ball_center_y + 100
+        self.shot_state = 0
+        self._start_time = time.time()
+        self._stop_game = False
+
+    def get_timeleft(self):
+        end_time = time.time()
+        return time_game - int((end_time - self._start_time))
+
 
 
     def on_show(self):
@@ -42,6 +62,7 @@ class Chapter3View(arcade.View):
             arcade.draw_text("Penalty Game", settings.WIDTH/2, settings.HEIGHT/2,
                          arcade.color.BLACK, font_size=30, anchor_x="center")
         
+        
             
 
         
@@ -50,15 +71,16 @@ class Chapter3View(arcade.View):
             arcade.draw_texture_rectangle(settings.WIDTH/2, settings.HEIGHT/2, settings.WIDTH, settings.HEIGHT, self.background)
             arcade.draw_circle_filled(self.ball_center_x, self.ball_center_y, 25, arcade.color.BLUE)
             self.goalie.draw()
-            
+
+            arcade.draw_line(self.ball_center_x, self.ball_center_y, self.end_ball_x, self.end_ball_y, arcade.color.WHITE)
         
     
             self.shooter.draw()
-    
+
+            arcade.draw_text("Time Left: " + str(self.get_timeleft()) + "seconds", 300, 10, arcade.color.BLACK, font_size = 30, anchor_x = "center")
     def on_update(self, delta_time):
         
 
-        self.goalie.center_x += self.goalie_speed_x
 
         if self.goalie.center_x < (settings.WIDTH/2) - 140:
             self.goalie_speed_x *= -1
@@ -71,15 +93,29 @@ class Chapter3View(arcade.View):
         self.shooter.center_x += self.shooter_speed_x
         self.shooter.center_y += self.shooter_speed_y
 
+        
         if self.shooter.center_x >= self.ball_center_x and self.shooter.center_y >= self.ball_center_y:
             self.shooter_speed_x = 0 
             self.shooter_speed_y = 0
 
         self.timer_start += delta_time
         
+        if self.end_ball_x < (settings.WIDTH/2) - 300:
+            self.radar_speed_x *= -1
+        
+        if self.end_ball_x > (settings.WIDTH/2) + 300:
+            self.radar_speed_x *= -1
+        
+        self.end_ball_x += self.radar_speed_x
 
-    
-
+        if self.end_ball_y < (settings.HEIGHT/2) - 200:
+            self.radar_speed_y *= -1
+        
+        if self.end_ball_y > (settings.HEIGHT/2) + 200:
+            self.radar_speed_y *= -1
+        
+        self.end_ball_y += self.radar_speed_y
+        
 
     def on_key_press(self, key, modifiers):
         #self.director.next_view()
@@ -87,10 +123,20 @@ class Chapter3View(arcade.View):
             self.menu = False
             self.in_game = True
         
-
             self.menu = False
             self.in_game = True
 
+    
+        if key == arcade.key.SPACE and self.shot_state == 0:
+            self.radar_speed_x = 0
+            self.radar_speed_y = 2
+            self.shot_state = 1
+        
+        elif key == arcade.key.SPACE and self.shot_state == 1:
+            self.radar_speed_y = 0
+            self.shot_state = 2
+            
+        
 if __name__ == "__main__":
     """This section of code will allow you to run your View
     independently from the main.py file and its Director.

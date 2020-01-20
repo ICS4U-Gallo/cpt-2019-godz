fa982fcd05ddd6cb20c543f7e6483e6f5ea73aba
amark02 <44906842+amark02@users.noreply.github.com>
2020-01-20 09:03:45 -0500

    Update chapter_2.py

--- chapter_2.py
+++ chapter_2.py
@@ -9,18 +9,30 @@ player_jump = 15
 player_friction = 0.8
 player_scaling = 0.4
 
-#Bullet constants
+#Bullet constants2
 bullet_scaling = 0.8
 bullet_speed = 10
 
 
 class Character(arcade.Sprite):
+    """Character class
+
+    Attrs:
+        filename (str) = name of the resource pack for character image
+        center_x (int) = x position of the character
+        center_y (int) = y position of the character
+        scale (int) = scaling size of the image for the character
+    """
+
     def __init__(self, filename, x, y):
         super().__init__(filename=filename, center_x=x, center_y=y, scale=player_scaling)
+
+        # The amount of height and zoom when the character is being followed across the map
         self.camera_zoom = 500
         self.camera_height = 40
 
     def update_map_view(self):
+        """Returns the x, x1, y, y1 positions for the window view"""
         x = self.center_x - self.camera_zoom
         x1 = self.center_x + self.camera_zoom
         y= self.camera_height - self.camera_zoom
@@ -30,15 +42,21 @@ class Character(arcade.Sprite):
 
         
 class Platforms:
+    """Platform class"""
+
     def __init__(self):
+        # Make a list of the platforms
         self.all = arcade.SpriteList()
+
+        # Constants for the platforms
         self.resource_pack = ":resources:/images/tiles/stoneMid.png"
         self.sprite_scaling = 0.2
 
         self.sprite_size_w = 128
         self.sprite_size_h = 128
 
-    def recursive_platforms(self, y, range_list):   
+    def recursive_platforms(self, y, range_list): 
+        """Funcction that makes platforms for the character to land on"""
         s = arcade.Sprite(filename=self.resource_pack, center_x= range_list[0], center_y=y, scale=self.sprite_scaling)
         self.all.append(s)
 
@@ -51,6 +69,13 @@ class Platforms:
 
 
 class Bullet(arcade.Sprite):
+    """Bullet class
+
+    Attrs:
+        filename (str): name of the resource pack for the bullet image
+        view_pointer (class): 
+    """
+
     def __init__(self, filename, view_pointer):
         self.view_pointer = view_pointer
         x = self.view_pointer.camera_position[1] + 15
@@ -62,43 +87,46 @@ class Bullet(arcade.Sprite):
 class Chapter2View(arcade.View):
     def __init__(self):
         super().__init__()
+        # In menu, in game, or game over
         self.menu = True
         self.in_game = False
+        self.game_over = False
 
-        #Player image
+        # Player image
         self.character_height = 128*0.4
         self.player = Character(":resources:/images/animated_characters/male_adventurer/maleAdventurer_idle.png", x=100, y=100)
 
-        #Platforms 
+        # Platforms 
         self.platform_manager = Platforms()
 
         self.platform_manager.recursive_platforms(y=50, range_list=[0,150])
         self.platform_manager.recursive_platforms(y=200, range_list=[500,1000])
         self.platform_manager.recursive_platforms(y=100, range_list=[170, 400])
-        self.platform_manager.recursive_platforms(y=300, range_list=[1100, 1800])
+        self.platform_manager.recursive_platforms(y=300, range_list=[1100, 1500])
+        self.platform_manager.recursive_platforms(y=100, range_list=[1600, 1900])
+        self.platform_manager.recursive_platforms(y=200, range_list=[2050, 2300])
+        self.platform_manager.recursive_platforms(y=350, range_list=[2400, 2700])
+        self.platform_manager.recursive_platforms(y=100, range_list=[2750, 3000])
 
-        #Player movement
+        # Player movement
         self.key_pressed = {
             arcade.key.W: False,
             arcade.key.A: False,
             arcade.key.D: False, arcade.key.ENTER: False}  
         
-        #Physics
+        # Physics
         self.physics = arcade.PhysicsEnginePlatformer(self.player, self.platform_manager.all)   
-        self.physics.gravity_constant = 0.7
+        self.physics.gravity_constant = 0.5
 
-        #Camera position
+        # Camera position
         self.camera_position = None
 
-        #Bullet list
+        # Bullet list
         self.bullet_list = arcade.SpriteList()
         self.bullet_sprite = ":resources:images/space_shooter/laserBlue01.png"
 
     def on_show(self):
-        arcade.set_background_color(arcade.color.WHITE)
-
-    def setup(self):
-        pass 
+        arcade.set_background_color(arcade.color.WHITE) 
 
     def on_draw(self):
         arcade.start_render()
@@ -117,12 +145,31 @@ class Chapter2View(arcade.View):
             self.player.draw()
             self.platform_manager.all.draw()
 
-            #for bullet in self.bullet_list:
+            # for bullet in self.bullet_list:
                 #bullet.draw()
             self.bullet_list.draw()
+        
+        if self.game_over is True:
+            arcade.set_background_color(arcade.color.BLACK)
+            arcade.draw_text("Game Over", 100, settings.HEIGHT/2,
+                         arcade.color.WHITE, font_size=30, anchor_x="center")
+            arcade.draw_text("Game Over", 500, settings.HEIGHT/2,
+                         arcade.color.WHITE, font_size=30, anchor_x="center")
+            arcade.draw_text("Game Over", 900, settings.HEIGHT/2,
+                         arcade.color.WHITE, font_size=30, anchor_x="center")
+            arcade.draw_text("Game Over", 1300, settings.HEIGHT/2,
+                         arcade.color.WHITE, font_size=30, anchor_x="center")
+            arcade.draw_text("Game Over", 1700, settings.HEIGHT/2,
+                         arcade.color.WHITE, font_size=30, anchor_x="center")
+            arcade.draw_text("Game Over", 2100, settings.HEIGHT/2,
+                         arcade.color.WHITE, font_size=30, anchor_x="center")
+            arcade.draw_text("Game Over", 2500, settings.HEIGHT/2,
+                         arcade.color.WHITE, font_size=30, anchor_x="center")
+            arcade.draw_text("Game Over", 2900, settings.HEIGHT/2,
+                         arcade.color.WHITE, font_size=30, anchor_x="center")
 
     def on_key_press(self, key, modifiers):
-        #self.director.next_view()
+        # self.director.next_view()
 
         self.key_pressed[key] = True
     
@@ -132,7 +179,7 @@ class Chapter2View(arcade.View):
     def on_update(self, delta_time):
         self.physics.update()
 
-        #Player movement with keys
+        # Player movement with keys
         if self.key_pressed[arcade.key.ENTER] == True:
             self.menu = False
             self.in_game = True
@@ -148,12 +195,12 @@ class Chapter2View(arcade.View):
 
         self.player._set_change_x(self.player._get_change_x()*player_friction)
 
-        #Falls off map
+        # Falls off map
         if self.player.center_y <= -500:
             self.player.center_x = 100
             self.player.center_y = 100
 
-        #Map scrolling
+        # Map scrolling
         if self.in_game is True:
             x, x1, y, y1 = self.player.update_map_view()
 
@@ -161,15 +208,19 @@ class Chapter2View(arcade.View):
 
             self.camera_position = [x, x1, y, y1]
 
-        #Bullet spawning
+        # Bullet spawning
         for bullet in self.bullet_list:
             bullet.change_x = -bullet_speed
         if round(time.time(), 1) % 10 == 0 and self.in_game is True:
             self.bullet_list.append(Bullet(self.bullet_sprite, self))
-                
-
-        if len(self.bullet_list) > 0:
-            print(self.bullet_list[0]._get_change_x())
+        
+        # Bullet collides with character
+        if self.player.collides_with_list(self.bullet_list):
+            self.game_over = True
+        
+        # Player gets to the last platform
+        #if self.player.collides_with_Sprite(self.platform_manager):
+          #self.game_over = True
 
         
 if __name__ == "__main__":
@@ -188,6 +239,5 @@ if __name__ == "__main__":
 
     my_view = Chapter2View()
     my_view.director = FakeDirector(close_on_next_view=True)
-    my_view.setup()
     window.show_view(my_view)
     arcade.run()
